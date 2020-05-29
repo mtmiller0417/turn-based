9b2df60dc22d4f6b5dcaecf47278b8c2b631ad43
Matt <matt.miller041798@gmail.com>
2020-05-28 23:29:35 -0400

    initial commit

---
+++ config/webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
+const package = require('../package.json');
+
+module.exports = {
+  entry: ['./src/scripts/game.ts'],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, include: path.join(__dirname, '../src'), loader: 'ts-loader' }]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          filename: '[name].bundle.js'
+        }
+      }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ gameName: package.game.name, template: 'src/index.html' }),
+    new CopyWebpackPlugin([
+      { from: 'src/assets', to: 'assets' },
+      { from: 'config/pwa', to: '' },
+      { from: 'src/assets/icons/favicon.ico', to: '' }
+    ]),
+    new InjectManifest({
+      swSrc: path.resolve(__dirname, 'pwa/sw.js')
+    })
+  ]
+};
